3e42b0cfee2326f4c88d4a21bf5b897fae848cc1
Tao <tao.galasse@gmail.com>
2019-10-20 23:43:43 +0200

    create users table in DB & set up doorkeeper

---
+++ db/migrate/20191020213500_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :encrypted_password, limit: 128, null: false
+      t.string :confirmation_token, limit: 128
+      t.string :remember_token, limit: 128, null: false
+
+      t.string :role
+      t.string :last_name
+      t.string :first_name
+      t.string :type
+
+      t.timestamps
+    end
+
+    add_index :users, :email
+    add_index :users, :remember_token
+  end
+end
