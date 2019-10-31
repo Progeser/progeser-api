c7d4385ace81950bf3a52bbc13524d30ec6fcb3a
Tao <tao.galasse@gmail.com>
2019-10-31 01:04:02 +0100

    set up rspec_api_documentation with swagger-ui + add documentation for sign in endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  use_doorkeeper
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api do
+    scope :v1 do
+      use_doorkeeper
+    end
+  end
 end
