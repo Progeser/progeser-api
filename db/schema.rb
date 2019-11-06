702e90a3f6a2b289629dd1403d9c60144a0ddfac
Tao <tao.galasse@gmail.com>
2019-11-06 01:20:22 +0100

    chore: fix annotate config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_151337) do
+ActiveRecord::Schema.define(version: 2019_11_05_233037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,8 +63,8 @@ ActiveRecord::Schema.define(version: 2019_10_31_151337) do
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
     t.string "role"
-    t.string "last_name"
     t.string "first_name"
+    t.string "last_name"
     t.string "type"
     t.string "laboratory"
     t.datetime "created_at", null: false
