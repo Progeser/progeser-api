14d98a547e937c5cb0a3a70f6a26c8673e507114
David Lelièvre <david72.lelievre@free.fr>
2024-10-30 08:45:34 +0100

    ci

--- db/migrate/20241015112508_add_building_to_greenhouses.rb
+++ db/migrate/20241015112508_add_building_to_greenhouses.rb
@@ -4,7 +4,7 @@ class AddBuildingToGreenhouses < ActiveRecord::Migration[7.2]
   def up
     add_reference :greenhouses, :building, foreign_key: true
 
-    building = Building.create!(name: 'Default Building', location: 'Default Location')
+    building = Building.create!(name: 'Default Building', description: 'Default Location')
     Greenhouse.find_each do |greenhouse|
       greenhouse.update!(building:)
     end
