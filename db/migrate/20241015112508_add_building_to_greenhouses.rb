d1dd00cbb43099ebcaa5802d850853765335295e
Lelievre-david <81243462+Lelievre-david@users.noreply.github.com>
2024-10-30 08:26:33 +0100

    Update db/migrate/20241015112508_add_building_to_greenhouses.rb
    Co-authored-by: taogt <tao.galasse@gmail.com>

--- db/migrate/20241015112508_add_building_to_greenhouses.rb
+++ db/migrate/20241015112508_add_building_to_greenhouses.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
-class AddBuildingToGreenhouses < ActiveRecord::Migration[6.0]
-  def change
+class AddBuildingToGreenhouses < ActiveRecord::Migration[7.2]
+  def up
     add_reference :greenhouses, :building, foreign_key: true
 
-    reversible do |dir|
-      dir.up do
-        building = Building.create!(name: 'Default Building', location: 'Default Location')
-
-        Greenhouse.find_each do |greenhouse|
-          greenhouse.update!(building:)
-        end
-      end
+    building = Building.create!(name: 'Default Building', location: 'Default Location')
+    Greenhouse.find_each do |greenhouse|
+      greenhouse.update!(building:)
     end
+  end
+
+  def down
+    remove_reference :greenhouses, :building, foreign_key: true
   end
 end
