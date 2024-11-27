22da4cbaeb80a3f01154d045e3ca723e1d11a116
David Lelièvre <david72.lelievre@free.fr>
2024-11-27 11:12:10 +0100

    udpate

--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_27_095600) do
 
   create_table "buildings", force: :cascade do |t|
     t.string "name"
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "greenhouses", force: :cascade do |t|
