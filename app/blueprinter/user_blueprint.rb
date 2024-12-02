7c65ed9dc58d0497c478b47854894063b7ae69ca
David Lelièvre <david72.lelievre@free.fr>
2024-12-02 14:33:24 +0100

    clean

--- app/blueprinter/user_blueprint.rb
+++ app/blueprinter/user_blueprint.rb
@@ -3,11 +3,4 @@
 class UserBlueprint < Base
   # Fields
   fields :email, :role, :first_name, :last_name, :laboratory
-
-  # Views
-  view :with_token do
-    association :access_tokens,
-                name: :token,
-                blueprint: TokenBlueprint
-  end
 end
